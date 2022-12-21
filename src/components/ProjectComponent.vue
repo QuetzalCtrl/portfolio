521e05a4b1cfde47eb2588b303a4442f945823fc
QuetzalCtrl <hugo.vanrobaeys@gmail.com>
2022-12-21 21:03:24 +0700

    test thm badge

--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -1,5 +1,16 @@
 <template>
     <div>test projet numéro {{ $route.params.projectId }}</div>
+    <div id="gist"></div>
+    
 </template>
+
 <script>
+/* eslint-disable no-useless-escape */
+import postscribe from 'postscribe'
+export default {
+  name: 'Gist-Example',
+  mounted: function () {
+    postscribe('#gist', `<script src="https://tryhackme.com/badge/185553"><\/script>`)
+  },
+}
 </script>
