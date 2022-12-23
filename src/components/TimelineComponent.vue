3ac709f10620d84bb4c2755e6c9e7876302d9b0c
QuetzalCtrl <hugo.vanrobaeys@gmail.com>
2022-12-23 20:27:20 +0700

    correction contenu du texte et reformulation d'idées

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -14,7 +14,7 @@
                 <div class="order-1 w-5/12 px-1 text-right">
                     <p class="mb-3 text-base text-green">July 2019</p>
                     <h4 class="mb-3 font-bold text-lg md:text-2xl">BAC S &#8226; Maths spe</h4>
-                    <TimelineText desc="Highscool, or get to know what you want to do 'when you grow up', right? Well, for me, it was an evidence: computer science, more specifically networking and cybersecurity, that's what I loved! So that's what I did :) I definitely wanted to get to know more than what I learned from my small website projects and CTF challenges I did at that time"/>
+                    <TimelineText desc="High school is a time to figure out what you want to do when you grow up, right? For me, it was clear: computer science, specifically networking and cybersecurity, is what I loved! So that's what I pursued. I definitely wanted to learn more than just what I learned from my small website projects and CTF challenges at that time."/>
                 </div>
                 </div>
                 <div class="flex justify-between items-center w-full right-timeline">
@@ -22,7 +22,7 @@
                 <div class="order-1  w-5/12 px-1 text-left">
                     <p class="mb-3 text-base text-green">Sept 2019 - July 2021</p>
                     <h4 class="mb-3 font-bold text-lg md:text-2xl">DUT info @ <a target="_blank" class="text-blue font-bold" href="https://www.iut-a.univ-lille.fr/en/home/">IUT A Lille</a></h4>
-                    <TimelineText desc="During those 2 years, I started to realize that computer science is a VERY vaste world! Web developpement, object-oriented programming, databases, networking, system administration,... I learned a lot of different computer-related notions and aspects! The biggest benefit I took advantage of learning all those concepts is that I learned what I really liked to do, and not to do. This comforted me, and pushed me to continue learning new cybersecurity and networking materials."/>
+                    <TimelineText desc="During those two years, I realized that computer science is a very vast world! Web development, object-oriented programming, databases, networking, system administration... I learned about a lot of different computer-related concepts and aspects! The biggest benefit of learning all these things was that I figured out what I really liked to do, and what I didn't. This gave me confidence and motivated me to continue learning more about cybersecurity and networking."/>
                 </div>
                 </div>
                 <div class="flex justify-between flex-row-reverse items-center w-full left-timeline">
@@ -30,7 +30,7 @@
                 <div class="order-1 w-5/12 px-1 text-right">
                     <p class="mb-3 text-base text-green">Sept 2020 - July 2021</p>
                     <h4 class="mb-3 font-bold text-lg md:text-2xl">Web Dev. @ <a target="_blank" href="https://andersontech.fr/" class="text-blue font-bold">Anderson Tech</a></h4>
-                    <TimelineText desc="My very first real job experience! I chose to do my 2nd year of DUT as a work-study program, so I worked as a Web Developer at Anderson Tech, a small tech startup company. Even if i know I don't want to be a web developer my whole carrer, I consider knowing how to create full stack web apps, how to manage and stock data, or even learn different languages and frameworks is essential and MUST be mastered before any security aspect. During this year, I learned to work by myself, to take decisions and to work with a deadline, real clients, and to adapt my skills to other developers as a member of a team."/>
+                    <TimelineText desc="My very first real job experience! I chose to do my second year of DUT as a work-study program, so I worked as a web developer at Anderson Tech, a small tech startup company. Even though I know I don't want to be a web developer my whole career, I believe it's essential to know how to create full stack web apps, how to manage and store data, and even learn different languages and frameworks before focusing on security aspects. During this year, I learned how to work independently, make decisions, and work with deadlines and real clients. I also learned how to adapt my skills to work with other developers as part of a team."/>
                 </div>
                 </div>
 
@@ -39,7 +39,7 @@
                 <div class="order-1  w-5/12 px-1 pb-16">
                     <p class="mb-3 text-base text-green">Sept 2021 - July 2022</p>
                     <h4 class="mb-3 font-bold  text-lg md:text-2xl text-left">Gap year</h4>
-                    <TimelineText desc="After my DUT, I chose to take a break from my studies, to focus on other things. This year has been very rewarding for me so far! In addition to having been able to travel a lot, I was able to concentrate on the realization of personal projects that were close to my heart, such as this portfolio, or even a musical project."/>
+                    <TimelineText desc="After my DUT, I chose to take a break from my studies to focus on other things. This year has been very rewarding for me so far! In addition to traveling a lot, I was able to concentrate on personal projects that were close to my heart, such as this portfolio or even a musical project."/>
                 </div>
                 </div>
             </div>
@@ -50,11 +50,11 @@
             <p class="text-green uppercase tracking-loose">work and experiences</p>
             <p class="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2 font-bold">About me</p>
             <p class="text-sm md:text-base text-gray-50 mb-4">
-            Here's my different experiences over time. You can also visit my LinkedIn profile where my complete background is detailed, and of course don't hesitate to get in touch with me there!
+                Here are my different experiences over time. You can also visit my LinkedIn profile where my complete background is detailed, and don't hesitate to get in touch with me there!
             </p>
             <a href="#"
             class="bg-transparent mr-auto hover:bg-green text-green hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border hover:border-transparent"> 
-            See on LinkedIn</a>
+            My LinkedIn</a>
         </div>
         </div>
     </div>
