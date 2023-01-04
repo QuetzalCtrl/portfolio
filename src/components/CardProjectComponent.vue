490387517ebe9e167af931245de4a216e78b6c3c
QuetzalCtrl <hugo.vanrobaeys@gmail.com>
2023-01-04 17:01:53 +0700

    ajout de vrai projets

--- src/components/CardProjectComponent.vue
+++ src/components/CardProjectComponent.vue
@@ -2,41 +2,68 @@
 <div class=" px-1 w-full my-4 mx-4 md:mx-0 md:my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
 
 <!-- Article -->
-<article class="p-6 overflow-hidden rounded-lg shadow-lg text-white bg-dark">
+<article class="transform transition hover:scale-105 border-2 border-lightgray-100 overflow-hidden rounded-lg shadow-lg bg-lightgray-100">
 
     <a href="#">
-        <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random">
+        <img alt="Placeholder" class="block h-auto w-full" :src="props.project.image">
     </a>
 
     <header class="flex items-center justify-between leading-tight p-2 md:p-4">
         <h1 class="text-lg">
-            <a class="no-underline hover:underline " href="#">
-                Exemple projet
+            <a class="no-underline font-bold " href="#">
+                {{props.project.name}}
             </a>
         </h1>
-        <p class="text-grey-darker text-sm">
-            A completer
-        </p>
+        <a target="_blank" :href="props.project.github">
+        <button class="font-semibold text-green inline-flex items-center space-x-2 rounded">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-8 h-8" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+            <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+            </svg>
+        </button>
+        </a>
     </header>
 
-    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-        <a class="flex items-center no-underline " href="#">
-            <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random">
-            <p class="ml-2 text-sm">
-                Parler des techs utilisées, illustrer avec des petits logo, et ouvrir une modal détaillée quand on clique sur une card. Faire un gif d'illustration, et mettre le lien vers le repo github.
-            </p>
-        </a>
-        <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-            <span class="hidden">Like</span>
-            <i class="fa fa-heart"></i>
-        </a>
-    </footer>
+    <div class="flex flex-col gap-2 p-2 md:p-4">
+        <p class=" text-sm">
+            {{props.project.desc}}
+        </p>
+        <div class="text-center">
+            <a href="#"
+            class=" bg-transparent mr-auto hover:bg-green text-green hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border hover:border-transparent">
+            Read more</a>
+        </div>
+    </div>
+    
 
 </article>
 <!-- END Article -->
+
+
+
+
+
+<!-- A PLACER DANS LA MODAL -->
+<!-- <footer class="bg-dark p-4">
+    <p class="text-center font-bold text-lightgray-100">
+        Tech & tools used
+    </p>
+    <div class="flex items-center justify-center gap-4 p-2">
+        <template v-for="tech in props.project.techs" :key="tech">
+            <img :alt="props.project.name" class="block h-10"  :src="require(`@/${tech}`)">  
+        </template>
+    </div>
+</footer> -->
+
+
+
+
 
 </div>
 </template>
 
 <script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  project: Object
+})
 </script>
--- src/components/ProjectsComponent.vue
+++ src/components/ProjectsComponent.vue
@@ -1,33 +1,84 @@
 <template>
 <section id="projects" class="font-montserrat">
   <h3 class="mt-12 text-center text-4xl text-white font-bold">My projects</h3>
-  <h3 class="my-6 text-center text-white ">My biggest projects</h3>
+  <h3 class="my-6 text-center text-white ">My main projects</h3>
 
   <div class="container my-12 mx-auto px-4 md:px-12">
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
       <template v-for="i in projects" :key="i">
-        <CardProjectComponent></CardProjectComponent>
+        <CardProjectComponent  v-bind:project="i"></CardProjectComponent>
       </template>
     </div>
   </div>
   <h3 class="mt-6 text-center text-2xl text-white font-bold">Alternate projects</h3>
-  <h3 class="mb-6 text-center text-white ">Some smaller projects</h3>
+  <h3 class="mb-6 text-center text-white ">Scripts and projects worth mentionning too</h3>
   
 
   <div class="flex flex-wrap justify-center gap-5">
-    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Smaller project example</h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">Ici, pas de gif de présentation, mais tjrs possibilité de voir le repo github.</p>
-    </a>
-    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Smaller project example</h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">Ici, pas de gif de présentation, mais tjrs possibilité de voir le repo github.</p>
-    </a>
+    <div class="transform transition hover:scale-105 text-center block max-w-sm p-6 bg-dark text-white rounded-lg shadow-md border-2 border-lightgray-100">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight">What's In My PCAP</h5>
+      <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">A website made for PCAP data extraction, analysing and visualisation rendering in charts and graphs.</p>
+      <a href="https://github.com/QuetzalCtrl/QuetzalTheRipper"
+          class="bg-transparent mr-auto text-white hover:text-dark hover:bg-white rounded shadow hover:shadow-lg py-2 px-4 border hover:border-transparent">
+          See on github
+      </a>
+    </div>
+    <div class="transform transition hover:scale-105 text-center block max-w-sm p-6 bg-dark text-white rounded-lg shadow-md border-2 border-lightgray-100">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight ">Quetzal the Ripper</h5>
+      <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">Quetzal the Ripper is a Password cracker (my JohnTheRipper homemade version), created for educational purposes only.</p>
+      <a href="https://github.com/QuetzalCtrl/QuetzalTheRipper"
+          class="bg-transparent mr-auto text-white hover:text-dark hover:bg-white rounded shadow hover:shadow-lg py-2 px-4 border hover:border-transparent">
+          See on github
+      </a>
+    </div>
   </div>
 </section>
 </template>
 
 <script setup>
 import CardProjectComponent from './CardProjectComponent.vue';
-const projects = [1, 2, 3]
+const projects = [
+  {
+    name: "Vuln_ERR_able challenge",
+    desc: "A simple CTF challenge where you'll be confronted to common web exploitation, enumeration, and some basic privesc.",
+    github: "https://github.com/QuetzalCtrl/Vuln_ERR_able-CTF-Challenge/",
+    image: "https://user-images.githubusercontent.com/58345798/210528518-a7333581-3ff7-495c-91ab-d2b09fe59f8a.png",
+    techs: ["assets/tech/vagrant.png", "assets/tech/virtualbox.png", "assets/tech/nginx.png", "assets/tech/flask.png", "assets/tech/bash.png"],
+    gif: "https://user-images.githubusercontent.com/58345798/210528518-a7333581-3ff7-495c-91ab-d2b09fe59f8a.png"
+  }, 
+  {
+    name: "Catch Me If You Can",
+    desc: "Catch Me If You Can (or ifucan) is a tool that performs automated IP rotation using proxies or VPNs on Linux systems.",
+    github: "https://github.com/QuetzalCtrl/CatchMeIfYouCan",
+    image: "https://user-images.githubusercontent.com/58345798/210527533-c440dde1-c891-452a-a518-b874c8abd124.png",
+    techs: ["assets/tech/bash.png", "assets/tech/protonvpn.png", "assets/tech/cyberghost.png", "assets/tech/windscribe.png"],
+    gif: "https://user-images.githubusercontent.com/58345798/210527642-79f0a6f6-2f69-40f7-9cce-f308d71935cb.gif"
+  },
+  {
+    name: "Sokoban",
+    desc: "A puzzle game that runs in a terminal. You'll have to push the boxes around in a warehouse, trying to get them to storage locations.",
+    github: "https://github.com/QuetzalCtrl/Sokoban",
+    image: "https://user-images.githubusercontent.com/58345798/210526459-8bc9776e-3833-4bc8-a463-e8a13fcf753a.png",
+    techs: ["assets/tech/c.png", "assets/tech/makefile.png"],
+    gif: "https://user-images.githubusercontent.com/58345798/210526525-41f4d515-93dc-451e-a23c-d33fc2b05ba3.gif"
+  }
+]
+// const alt_projects = [
+//   {
+//     name: "Catch Me If You Can",
+//     desc: "Catch Me If You Can (or ifucan) is a tool that performs automated IP rotation using proxies or VPNs on Linux systems.",
+//     github: "https://github.com/QuetzalCtrl/CatchMeIfYouCan",
+//     image: "assets/projects/ifucan.png",
+//     techs: ["../assets/tech/bash.png", "../assets/tech/protonvpn.png", "../assets/tech/cyberghostvpn.png", "../assets/tech/windscribe.png"],
+//     gif: "assets/projects/ifucan.png"
+//   },
+//   {
+//     name: "Quetzal the Ripper",
+//     desc: "Quetzal the Ripper is a Password cracker (my JohnTheRipper homemade version), created for educational purposes only.",
+//     github: "https://github.com/QuetzalCtrl/QuetzalTheRipper",
+//     image: "assets/projects/quetzal.png",
+//     techs: ["../assets/tech/bash.png", "../assets/tech/openssl.png"],
+//     gif: "assets/projects/quetzal.png"
+//   }
+// ]
 </script>
