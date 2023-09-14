2d02e627f5fa716c39a57703a8b4e6a2e6a2692d
QuetzalCtrl <hugo.vanrobaeys@gmail.com>
2023-09-14 23:07:07 +0200

    fixed timeline dates

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -12,7 +12,7 @@
                 <div class="flex justify-between flex-row-reverse items-center w-full left-timeline">
                 <div class="order-1 w-5/12"></div>
                 <div class="order-1 w-5/12 px-1 text-right">
-                    <p class="mb-3 text-base text-green">July 2019</p>
+                    <p class="mb-3 text-base text-green">July 2020</p>
                     <h4 class="mb-3 font-bold text-lg md:text-2xl">BAC S &#8226; Maths spe</h4>
                     <TimelineText desc="High school is a time to figure out what you want to do when you grow up, right? For me, it was clear: computer science, specifically networking and cybersecurity, is what I loved! So that's what I pursued. I definitely wanted to learn more than just what I learned from my small website projects and CTF challenges at that time."/>
                 </div>
@@ -20,7 +20,7 @@
                 <div class="flex justify-between items-center w-full right-timeline">
                 <div class="order-1 w-5/12"></div>
                 <div class="order-1  w-5/12 px-1 text-left">
-                    <p class="mb-3 text-base text-green">Sept 2019 - July 2021</p>
+                    <p class="mb-3 text-base text-green">Sept 2020 - July 2022</p>
                     <h4 class="mb-3 font-bold text-lg md:text-2xl">DUT info @ <a target="_blank" class="text-blue font-bold" href="https://www.iut-a.univ-lille.fr/en/home/">IUT A Lille</a></h4>
                     <TimelineText desc="During those two years, I realized that computer science is a very vast world! Web development, object-oriented programming, databases, networking, system administration... I learned about a lot of different computer-related concepts and aspects! The biggest benefit of learning all these things was that I figured out what I really liked to do, and what I didn't. This gave me confidence and motivated me to continue learning more about cybersecurity and networking."/>
                 </div>
@@ -28,7 +28,7 @@
                 <div class="flex justify-between flex-row-reverse items-center w-full left-timeline">
                 <div class="order-1 w-5/12"></div>
                 <div class="order-1 w-5/12 px-1 text-right">
-                    <p class="mb-3 text-base text-green">Sept 2020 - July 2021</p>
+                    <p class="mb-3 text-base text-green">Sept 2021 - July 2022</p>
                     <h4 class="mb-3 font-bold text-lg md:text-2xl">Web Dev. @ <a target="_blank" href="https://andersontech.fr/" class="text-blue font-bold">Anderson Tech</a></h4>
                     <TimelineText desc="My very first real job experience! I chose to do my second year of DUT as a work-study program, so I worked as a web developer at Anderson Tech, a small tech startup company. Even though I know I don't want to be a web developer my whole career, I believe it's essential to know how to create full stack web apps, how to manage and store data, and even learn different languages and frameworks before focusing on security aspects. During this year, I learned how to work independently, make decisions, and work with deadlines and real clients. I also learned how to adapt my skills to work with other developers as part of a team."/>
                 </div>
@@ -37,7 +37,7 @@
                 <div class="flex justify-between items-center w-full right-timeline">
                 <div class="order-1 w-5/12"></div>
                 <div class="order-1  w-5/12 px-1">
-                    <p class="mb-3 text-base text-green">Sept 2021 - July 2022</p>
+                    <p class="mb-3 text-base text-green">Sept 2022 - July 2023</p>
                     <h4 class="mb-3 font-bold  text-lg md:text-2xl text-left">Gap year</h4>
                     <TimelineText desc="After my DUT, I chose to take a break from my studies to focus on other things. This year has been very rewarding for me! In addition to traveling a lot, I was able to concentrate on personal projects that were close to my heart, such as this portfolio or even a musical project."/>
                 </div>
